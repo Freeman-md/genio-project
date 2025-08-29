226611e285af5ff15509354ab5bc351fed46d013
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-08-29 14:56:51 +0100

    style(service-card): update text color class for improved theme consistency

--- src/app/components/sections/home/services/service-card.tsx
+++ src/app/components/sections/home/services/service-card.tsx
@@ -36,7 +36,7 @@ function ServiceCard({
       >
         <span
           className="
-            text-white sm:font-semibold
+            text-foreground sm:font-semibold
             text-xs sm:text-base md:text-lg
           "
         >
